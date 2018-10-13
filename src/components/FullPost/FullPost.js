904db640c82accf5d290c8aa6d1a956f286a09b7
agdavid <antonio.g.david@gmail.com>
2018-10-13 12:44:16 -0400

    DELETE data using axios

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -25,6 +25,13 @@ class FullPost extends Component {
         }
     };
 
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then( response => {
+                console.log(response);
+            });
+    }
+
     render () {
         // default 
         let post = <p style={{ textAlign: 'center'}}>Please select a Post!</p>;
@@ -39,7 +46,9 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button 
+                            className="Delete"
+                            onClick={this.deletePostHandler}>Delete</button>
                     </div>
                 </div>
     
