da689fcc7cf96da715e3077f6d4e9d8f3abb76e9
agdavid <antonio.g.david@gmail.com>
2018-10-13 12:08:57 -0400

    render single post using GET request

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p style={{ textAlign: 'center'}}>Please select a Post!</p>;
-        
+
+    state = {
+        loadedPost: null,
+    }
+
+    componentDidUpdate () {
         if (this.props.id) {
+            axios.get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+                .then( response => {
+                    this.setState({
+                        loadedPost: response.data,
+                    })
+                });
+        }
+    };
+
+    render () {
+        // default 
+        let post = <p style={{ textAlign: 'center'}}>Please select a Post!</p>;
+        // render when id is valid, but async request loading
+        if (this.props.id) {
+            post = <p style={{ textAlign: 'center'}}>Loading!</p>;
+        }
+        // render when id has been used to load post
+        if (this.state.loadedPost) {
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
