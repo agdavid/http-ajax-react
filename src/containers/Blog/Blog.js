27ba3f078eed75581d5cfccdd62a8434e1b2a480
agdavid <antonio.g.david@gmail.com>
2018-10-13 11:46:20 -0400

    render GET request data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,37 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    // set state with empty posts
+    state = {
+        posts: [],
+    }
+
+    // cause side effects after rendering component
+    componentDidMount () {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                // update state
+                this.setState({
+                    posts: response.data,
+                })
+            });
+    }
+
     render () {
+
+        const posts = this.state.posts.map(post => {
+            return (
+                <Post
+                    key={post.id} 
+                    title={post.title}/>
+            );
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
