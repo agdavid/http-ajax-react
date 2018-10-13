fadc18e35b076622ea468cf7c13f50b3fc8ce48b
agdavid <antonio.g.david@gmail.com>
2018-10-13 12:49:23 -0400

    example of error handling

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -12,6 +12,7 @@ class Blog extends Component {
     state = {
         posts: [],
         selectedPostId: null,
+        error: false
     }
 
     // cause side effects after rendering component
@@ -28,6 +29,10 @@ class Blog extends Component {
                 this.setState({
                     posts: updatedPosts,
                 })
+            })
+            .catch( error => {
+                console.log(error);
+                this.setState({error: true})
             });
     }
 
@@ -38,16 +43,18 @@ class Blog extends Component {
     };
 
     render () {
-
-        const posts = this.state.posts.map(post => {
-            return (
-                <Post
-                    key={post.id} 
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)}/>
-            );
-        });
+        let posts = <p style={{textAlign: 'center'}}> Something went wrong</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return (
+                    <Post
+                        key={post.id} 
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)}/>
+                );
+            });
+        }
 
         return (
             <div>
