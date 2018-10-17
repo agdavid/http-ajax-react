901aa632eabf8b3c3d1396ea35174a7d04c85714
agdavid <antonio.g.david@gmail.com>
2018-10-17 08:28:51 -0400

    refactor components into containers for routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,62 +1,10 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-import axios from '../../axios.js'; // import axios instance
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import Posts from './Posts/Posts';
 import './Blog.css';
 
 class Blog extends Component {
-
-    // set state with empty posts
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-    // cause side effects after rendering component
-    componentDidMount () {
-        axios.get('/posts')
-            .then(response => {
-                // update state
-                const posts = response.data.slice(0,4);
-                const updatedPosts = posts.map(post => {
-                    return { 
-                        ...post, 
-                        author: 'Max'};
-                })
-                this.setState({
-                    posts: updatedPosts,
-                })
-            })
-            .catch( error => {
-                console.log(error);
-                this.setState({error: true})
-            });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({
-            selectedPostId: id, 
-        })
-    };
-
     render () {
-        let posts = <p style={{textAlign: 'center'}}> Something went wrong</p>
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return (
-                    <Post
-                        key={post.id} 
-                        title={post.title}
-                        author={post.author}
-                        clicked={() => this.postSelectedHandler(post.id)}/>
-                );
-            });
-        }
-
         return (
             <div className="Blog">
                 <header>
@@ -67,15 +15,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                <Posts />
             </div>
         );
     }
