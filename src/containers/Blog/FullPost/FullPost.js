37f212376e1fc9ad7e890e0559f7a278e380d1bc
agdavid <antonio.g.david@gmail.com>
2018-10-26 10:09:04 -0700

    dynamic nested routes

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -4,54 +4,53 @@ import axios from 'axios';
 import './FullPost.css';
 
 class FullPost extends Component {
-
     state = {
-        loadedPost: null,
+        loadedPost: null
     }
 
     componentDidMount () {
-        if (this.props.match.params.id) {
-            // infinite loop caused by setState => componentDidUpdate => setState 
-            // add additional condition to setState
-            // only make request if: (1) no post selected; or (2) post selected and id is different
-            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-                axios.get('/posts/' + this.props.match.params.id)
+        console.log(this.props);
+        this.loadData();
+    }
+
+    componentDidUpdate() {
+        this.loadData();
+    }
+
+    loadData () {
+        if ( this.props.match.params.id ) {
+            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id) ) {
+                axios.get( '/posts/' + this.props.match.params.id )
                     .then( response => {
-                        this.setState({
-                            loadedPost: response.data,
-                        })
-                    });
+                        // console.log(response);
+                        this.setState( { loadedPost: response.data } );
+                    } );
             }
         }
-    };
+    }
 
     deletePostHandler = () => {
-        axios.delete('/posts/' + this.props.id)
-            .then( response => {
+        axios.delete('/posts/' + this.props.match.params.id)
+            .then(response => {
                 console.log(response);
             });
     }
 
     render () {
-        // default 
-        let post = <p style={{ textAlign: 'center'}}>Please select a Post!</p>;
-        // render when id is valid, but async request loading
-        if (this.props.id) {
-            post = <p style={{ textAlign: 'center'}}>Loading!</p>;
+        let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
+        if ( this.props.match.params.id ) {
+            post = <p style={{ textAlign: 'center' }}>Loading...!</p>;
         }
-        // render when id has been used to load post
-        if (this.state.loadedPost) {
+        if ( this.state.loadedPost ) {
             post = (
                 <div className="FullPost">
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button 
-                            className="Delete"
-                            onClick={this.deletePostHandler}>Delete</button>
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
                     </div>
                 </div>
-    
+
             );
         }
         return post;
